d474fe8b785b0def3a9b7c65a49288d5b494cbb8
minedetector <larsasi.la@hotmail.com>
2022-01-18 15:46:53 +0200

    Move the created account into the correct OU
    By default Accounts are created in the Root OU.
    With this the created Account is moved to the correct OU.

--- create-account.py
+++ create-account.py
@@ -4,25 +4,46 @@ import sys
 
 taltech_students_permission_set_arn = 'arn:aws:sso:::permissionSet/ssoins-650816aa11fd1188/ps-53d72fc1862a2e34'
 taltech_teachers_permission_set_arn = 'arn:aws:sso:::permissionSet/ssoins-650816aa11fd1188/ps-389b48caec9faba9'
+taltech_researchers_permission_set_arn = 'arn:aws:sso:::permissionSet/ssoins-650816aa11fd1188/ps-2a52296e98d0dc69'
 
-def main(account_owner_email, account_name):
+taltech_subjects_account_group_id = 'ou-f56r-yjx8ztgu'
+taltech_researchers_account_group_id = 'ou-f56r-mmg2wk9c'
+
+def main(account_owner_email, account_name, account_purpose):
+    if account_purpose not in ['subject', 'research']:
+        raise ValueError("Wrong account purpose, choose either subject or research")
+
     create_account(account_owner_email, account_name)
 
     instance_arn, instance_id = get_aws_sso_insatance_data()
 
     account_id = get_account_id(account_name)
 
-    students_group_name = f"UNISEC-{account_name}-students"
+    if account_purpose == "subject":
+        # Define the students group name that was created in the Active Directory
+        # The name has to match exactly otherwise this will fail
+        students_group_name = f"UNISEC-students-{account_name}"
 
-    students_group_id = get_group_id(students_group_name, instance_id)
+        students_group_id = get_group_id(students_group_name, instance_id)
 
-    assign_group_to_account(instance_arn, students_group_id, taltech_students_permission_set_arn, account_id)
+        assign_group_to_account(instance_arn, students_group_id, taltech_students_permission_set_arn, account_id)
 
-    teachers_group_name = f"UNISEC-{account_name}-teachers"
+        teachers_group_name = f"SEC-teachers-{account_name}"
 
-    teachers_group_id = get_group_id(teachers_group_name, instance_id)
+        teachers_group_id = get_group_id(teachers_group_name, instance_id)
 
-    assign_group_to_account(instance_arn, teachers_group_id, taltech_teachers_permission_set_arn, account_id)
+        assign_group_to_account(instance_arn, teachers_group_id, taltech_teachers_permission_set_arn, account_id)
+
+        move_account_to_group(account_id, taltech_subjects_account_group_id)
+
+    elif account_purpose == "research":
+        researchers_group_name = f"SEC-scientists-{account_name}"
+
+        researchers_group_id = get_group_id(researchers_group_name, instance_id)
+
+        assign_group_to_account(instance_arn, researchers_group_id, taltech_teachers_permission_set_arn, account_id)
+
+        move_account_to_group(account_id, taltech_researchers_account_group_id)
 
     print(account_id)
 
@@ -99,7 +120,19 @@ def assign_group_to_account(instance_arn, group_id, permission_set_arn, account_
 
     subprocess.run(command.split())
 
+# Moves the account from the Root OU (Organizational Unit in AWS Organizations) to the correct OU
+def move_account_to_group(account_id, destination_id):
+    command = f'''
+        aws organizations move-account \
+            --account-id {account_id} \
+            --source-parent-id r-f56r \
+            --destination-parent-id {destination_id}
+    '''
+    
+    subprocess.run(command.split())
+
 account_owner_email = sys.argv[1]
 account_name = sys.argv[2]
+account_purpose = sys.argv[3]
 
-main(account_owner_email, account_name)
+main(account_owner_email, account_name, account_purpose)
